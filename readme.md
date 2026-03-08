79853ec1692291858d0e75a91f430c3f065f0263
luoluobuli <joranlicn@gmail.com>
2026-03-08 17:59:50 -0400

    starter code

---
+++ readme.md
@@ -0,0 +1,37 @@
+# DynaMIX: Houdini Custom MPM Plugin
+
+## Overview
+
+## CMake Build Instructions
+### Preparation for building the project
+
+#### Set up Houdini environment variables
+
+You need to set two environment variables:
+HOUDINI_DSO_PATH
+%CUSTOM_DSO_PATH%;&
+
+CUSTOM_DSO_PATH
+D:\Documents\houdini\dso
+
+For CUSTOM_DSO_PATH, you can set it to any directory you want.
+
+#### Set up variables in CMakelists.txt
+
+You need to set `HOUDINI_INSTALL_PATH` to the path where Houdini is installed. For example, if Houdini is installed in `D:/Program Files/Side Effects Software/Houdini 21.0.596`, then you should set `HOUDINI_INSTALL_PATH` to `D:/Program Files/Side Effects Software/Houdini 21.0.596`.
+
+### Building the project using CMake
+Use CMake 3.24 or above to configure and generate the Visual Studio solution as follows:
+1. Open the Windows command prompt window
+2. In the command prompt window change to the directory where you have the CMakeLists.txt file
+3. At the Windows command prompt run the command
+`cmake -G "Visual Studio 17 2022" -A x64 -S .\ -B .\Build`
+4. In the generated solution, build the project.
+
+### Loading the Houdini Plugin
+- Move the cursor to the lower right panel in Houdini (Network Editor) and press the Tab key
+- Type "Geometry" then choose the geometry object.
+- Click to place in the Network Editor window.
+- Double-click the new geometry node to go into it.
+- Press Tab again in the Network Editor window and type "MyMPM".
+- Click to place.
---
+++ src/MPMPlugin.C
@@ -0,0 +1,131 @@
+#include <UT/UT_DSOVersion.h>
+//#include <RE/RE_EGLServer.h>
+
+
+#include <UT/UT_Math.h>
+#include <UT/UT_Interrupt.h>
+#include <GU/GU_Detail.h>
+#include <GU/GU_PrimPoly.h>
+#include <CH/CH_LocalVariable.h>
+#include <PRM/PRM_Include.h>
+#include <PRM/PRM_SpareData.h>
+#include <OP/OP_Operator.h>
+#include <OP/OP_OperatorTable.h>
+
+#include <iostream>
+#include <limits.h>
+#include "MPMPlugin.h"
+
+using namespace HDK_Sample;
+
+//
+// Help is stored in a "wiki" style text file. 
+//
+// See the sample_install.sh file for an example.
+//
+// NOTE : Follow this tutorial if you have any problems setting up your visual studio 2008 for Houdini 
+//  http://www.apileofgrains.nl/setting-up-the-hdk-for-houdini-12-with-visual-studio-2008/
+
+
+///
+/// newSopOperator is the hook that Houdini grabs from this dll
+/// and invokes to register the SOP.  In this case we add ourselves
+/// to the specified operator table.
+///
+void
+newSopOperator(OP_OperatorTable *table)
+{
+    table->addOperator(
+	    new OP_Operator("CusMPM",			// Internal name
+			    "MyMPM",			// UI name
+			     SOP_MPM::myConstructor,	// How to build the SOP
+			     SOP_MPM::myTemplateList,	// My parameters
+			     0,				// Min # of sources
+			     0,				// Max # of sources
+			     SOP_MPM::myVariables,	// Local variables
+			     OP_FLAG_GENERATOR)		// Flag it as generator
+	    );
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//PUT YOUR CODE HERE
+//You need to declare your parameters here
+//Example to declare a variable for angle you can do like this :
+//static PRM_Name		angleName("angle", "Angle");
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//				     ^^^^^^^^    ^^^^^^^^^^^^^^^
+//				     internal    descriptive version
+
+
+// PUT YOUR CODE HERE
+// You need to setup the initial/default values for your parameters here
+// For example : If you are declaring the inital value for the angle parameter
+// static PRM_Default angleDefault(30.0);	
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+PRM_Template
+SOP_MPM::myTemplateList[] = {
+// PUT YOUR CODE HERE
+// You now need to fill this template with your parameter name and their default value
+// EXAMPLE : For the angle parameter this is how you should add into the template
+// PRM_Template(PRM_FLT,	PRM_Template::PRM_EXPORT_MIN, 1, &angleName, &angleDefault, 0),
+// Similarly add all the other parameters in the template format here
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+    PRM_Template()
+};
+
+
+// Here's how we define local variables for the SOP.
+enum {
+	VAR_PT,		// Point number of the star
+	VAR_NPT		// Number of points in the star
+};
+
+CH_LocalVariable
+SOP_MPM::myVariables[] = {
+    { "PT",	VAR_PT, 0 },		// The table provides a mapping
+    { "NPT",	VAR_NPT, 0 },		// from text string to integer token
+    { 0, 0, 0 },
+};
+
+bool
+SOP_MPM::evalVariableValue(fpreal &val, int index, int thread)
+{
+	return false;
+}
+
+OP_Node *
+SOP_MPM::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
+{
+    return new SOP_MPM(net, name, op);
+}
+
+SOP_MPM::SOP_MPM(OP_Network *net, const char *name, OP_Operator *op)
+	: SOP_Node(net, name, op)
+{
+}
+
+SOP_MPM::~SOP_MPM() {}
+
+unsigned
+SOP_MPM::disableParms()
+{
+    return 0;
+}
+
+OP_ERROR
+SOP_MPM::cookMySop(OP_Context &context)
+{
+	fpreal now = context.getTime();
+	std::cerr << "Hello world! This is the initialization for MPM." << std::endl;
+    return error();
+}
+
